c50a167ae6f76a247ce6edafd06ee0bb744c0e8d
Gabeg30 <142956379+Gabeg30@users.noreply.github.com>
2023-10-02 13:55:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# working with apis and dictionaries
+
+import requests
+import json
+response = requests.get("https://randomuser.me/api")
+
+# print(response.json())
+
+title = response.json()['results'][0]['name']['first']
+print(title)
+gender = response.json()['results'][0]['gender']
+print(gender)
+last = response.json()['results'][0]['name']['last']
+print(last)
+street = response.json()['results'][0]['location']['street']['name']
+print(street)
+city= response.json()['results'][0]['location']['city']
+print(city)
+state = response.json()['results'][0]['location']['state']
+print(state)
+country = response.json()['results'][0]['location']['country']
+print(country)
+post_code = response.json()['results'][0]['location']['postcode']
+print(post_code)
+email = response.json()['results'][0]['email']
+print(email)
+user= response.json()['results'][0]['login']['username']
+print(user)
